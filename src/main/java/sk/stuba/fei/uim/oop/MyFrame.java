b40c73f96d337d2fc49f87f5f578ed19471db846
PeterTakacs3000 <xtakacsp@stuba.sk>
2021-06-03 14:26:46 +0200

    MyFrame

---
+++ src/main/java/sk/stuba/fei/uim/oop/MyFrame.java
@@ -0,0 +1,11 @@
+package sk.stuba.fei.uim.oop;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame(){
+        setSize(300, 300);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
